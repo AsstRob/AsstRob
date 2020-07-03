0d20f9ffd8a405202f5f2b012cc100c002871684
YasasDEK <ydilshan.ek@gmail.com>
2020-07-03 15:47:51 +0530

    motors initialized

---
+++ asst_rob/processing/New_run.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO 
+from time import sleep
+
+in1 = 24
+in2 = 23
+in3 = 17
+in4 = 27
+en = 25
+temp1=1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1,GPIO.OUT)
+GPIO.setup(in2,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(en,GPIO.OUT)
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+p=GPIO.PWM(en,1000)
+
+p.start(25)
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n") 
