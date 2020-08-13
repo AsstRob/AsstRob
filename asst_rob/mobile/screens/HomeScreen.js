a19697113abc921a73e0932f6c4a8a0adc5324a4
YasasDEK <ydilshan.ek@gmail.com>
2020-08-13 16:17:30 +0530

    multiple devices

--- asst_rob/mobile/screens/HomeScreen.js
+++ asst_rob/mobile/screens/HomeScreen.js
@@ -1,46 +1,45 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-alert */
 import React, {useState} from 'react';
-import { View, Text, Button, StyleSheet, StatusBar, Picker } from 'react-native';
-import { useTheme } from '@react-navigation/native';
+import {View, Text, Button, StyleSheet, StatusBar, Picker} from 'react-native';
+import {useTheme} from '@react-navigation/native';
 // import { Dropdown } from 'react-native-material-dropdown';
 
 const HomeScreen = ({navigation}) => {
-
-  const { colors } = useTheme();
+  const {colors} = useTheme();
 
   const theme = useTheme();
-  
+
   const selecedLabel = 0;
 
-  const [count,setCount] = useState(0)
-  const show = (value) => {
-    if(value == '00') {
+  const [count, setCount] = useState(0);
+  const show = value => {
+    if (value === '00') {
       alert("You did't select any device ");
-      setCount(value);  
-
-    }
-    else {
-      alert("You have selected the device " + value);
+      setCount(value);
+    } else {
+      alert('You have selected the device ' + value);
       setCount(value);
     }
-  }
-  
-    return (
-      <View>
+  };
+
+  return (
+    <View>
       <Picker onValueChange={show.bind()} selectedValue={count}>
-        <Picker.Item label="Select a device" value="00"></Picker.Item>
-        <Picker.Item label="Device 01" value="01"></Picker.Item>
-        <Picker.Item label="Device 02" value="02"></Picker.Item>
+        <Picker.Item label="Select a device" value="00" />
+        <Picker.Item label="Device 01" value="01" />
+        <Picker.Item label="Device 02" value="02" />
       </Picker>
-      </View>
-    );
+    </View>
+  );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- asst_rob/mobile/screens/SettingsScreen.js
+++ asst_rob/mobile/screens/SettingsScreen.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-alert */
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet, Picker, Image} from 'react-native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const SettingsScreen = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Settings Screen</Text>
-        <Button
-          title="Click Here"
-          onPress={() => alert('Button Clicked!')}
-        />
+const settingsScreen = ({navigation}) => {
+  const [count, setCount] = useState(0);
+  const show = value => {
+    if (value === '00') {
+      alert("You did't select any device ");
+      setCount(value);
+    } else {
+      alert('You have selected the device ' + value);
+      setCount(value);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Icon.Button
+        name="ios-arrow-back"
+        size={25}
+        backgroundColor="#009387"
+        onPress={() => navigation.navigate('HomeDrawer')}
+      />
+      <Picker onValueChange={show.bind()} selectedValue={count}>
+        <Picker.Item label="Select a device" value="00" />
+        <Picker.Item label="Device 01" value="01" />
+        <Picker.Item label="Device 02" value="02" />
+      </Picker>
+      <View style={styles.image}>
+        <Image resizeMode="center" source={require('../assets/test.png')} />
       </View>
-    );
+    </View>
+  );
 };
 
-export default SettingsScreen;
+export default settingsScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    // justifyContent: 'center',
+    // flex: 1,
+    // marginTop: 100,
+    // alignItems: 'center',
+  },
+  image: {
+    // marginTop: -20,
+    alignItems: 'center',
+  },
+  full: {
+    // marginBottom: 200,
   },
 });
