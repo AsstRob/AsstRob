ebd7efc3b86c05d43ccda20c48017b604129b56b
migara96 <47963309+migara96@users.noreply.github.com>
2020-10-05 05:09:30 +0530

    Update collision_detector.py

--- asst_rob/core/navigation/helpers/collision_detector.py
+++ asst_rob/core/navigation/helpers/collision_detector.py
@@ -7,15 +7,14 @@ SAFE_DISTANCE = 30
 #add pins
 
 #left sensor
-TRIGGER_1 = 18
-ECHO_1 = 24
+TRIG_1 = 17
+ECHO_1 = 4
 #mid sensor
-TRIGGER_2 = 18
-ECHO_2 = 24
+TRIG_2 = 22
+ECHO_2 = 5
 #right sensor
-TRIGGER_3 = 18
-ECHO_3 = 24
-
+TRIG_3 = 18
+ECHO_3 = 23
 
 class CollisionDetector(BaseNavigator):
 
@@ -26,49 +25,76 @@ class CollisionDetector(BaseNavigator):
         self.__side = Directions.NONE               # Which side of the path the asstrob is on: 0 - center, 1 - left, 2 - right
 
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
-        GPIO.setup(TRIGGER_1, GPIO.OUT)
-        GPIO.setup(ECHO_1, GPIO.IN)
-        GPIO.setup(TRIGGER_2, GPIO.OUT)
-        GPIO.setup(ECHO_2, GPIO.IN)
-        GPIO.setup(TRIGGER_3, GPIO.OUT)
-        GPIO.setup(ECHO_3, GPIO.IN)
+        GPIO.setup(ECHO_1,GPIO.IN )
+        GPIO.setup(TRIG_1, GPIO.OUT )
+        GPIO.setup(ECHO_2,GPIO.IN )
+        GPIO.setup(TRIG_2, GPIO.OUT )
+        GPIO.setup(ECHO_3,GPIO.IN )
+        GPIO.setup(TRIG_3, GPIO.OUT )
 
     def __get_collision_data(self):
-        GPIO.output(TRIGGER_1, True)
-        GPIO.output(TRIGGER_2, True)
-        GPIO.output(TRIGGER_3, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIGGER_1, False)
-        GPIO.output(TRIGGER_2, False)
-        GPIO.output(TRIGGER_3, False)
+        while True:
+            GPIO.output(TRIG_1, GPIO.LOW)
+            time.sleep(MEASURE_INTERVAL_TIME); #DELAY
+            GPIO.output(TRIG_1, GPIO.HIGH)
+            time.sleep(SENSOR_SETTLE_TIME)
+            GPIO.output(TRIG_1, GPIO.LOW)
+            while GPIO.input(ECHO_1) == 0:
+                start_1 = time.time()
+            while GPIO.input(ECHO_1) == 1:
+                end_1 = time.time()
 
-        while GPIO.input(ECHO_1) == 0:
-            start_1 = time.time()
-        while GPIO.input(ECHO_2) == 0:
-            start_2 = time.time()
-        while GPIO.input(ECHO_3) == 0:
-            start_3 = time.time()
-        while GPIO.input(ECHO_1) == 1:
-            end_1 = time.time()
-        while GPIO.input(ECHO_2) == 1:
-            end_2 = time.time()
-        while GPIO.input(ECHO_3) == 1:
-            end_3 = time.time()
+            duration_1 = end_1 - start_1
 
-        duration_1 = end_1-start_1
-        duration_2 = end_2-start_2
-        duration_3 = end_3-start_3
+            distance_1 = duration_1 * MEASURE_REFERENCE
+            distanceRound_1 = round(distance_1, 2)
 
-        distance_1 = round(duration_1*17150, 2)
-        distance_2 = round(duration_2*17150, 2)
-        distance_3 = round(duration_3*17150, 2)
+            print("Distance of sensor 1 :", distanceRound_1, "cm")
 
-        left_val = (distance_1 < SAFE_DISTANCE)
-        mid_val = (distance_2 < SAFE_DISTANCE)
-        right_val = (distance_3 < SAFE_DISTANCE)
+            GPIO.output(TRIG_2, GPIO.LOW)
+            time.sleep(MEASURE_INTERVAL_TIME); #DELAY
+            GPIO.output(TRIG_2, GPIO.HIGH)
+            time.sleep(SENSOR_SETTLE_TIME)
+            GPIO.output(TRIG_2, GPIO.LOW)
+            while GPIO.input(ECHO_2) == 0:
+                start_2 = time.time()
+            while GPIO.input(ECHO_2) == 1:
+                end_2 = time.time()
 
-        return left_val, mid_val, right_val
+            duration_2 = end_2 - start_2
+
+            distance_2 = duration_2 * MEASURE_REFERENCE
+            distanceRound_2 = round(distance_2, 2)
+
+            print("Distance of sensor 2 :", distanceRound_2, "cm")
+
+            GPIO.output(TRIG_3, GPIO.LOW)
+            time.sleep(MEASURE_INTERVAL_TIME); #DELAY
+            GPIO.output(TRIG_3, GPIO.HIGH)
+            time.sleep(SENSOR_SETTLE_TIME)
+            GPIO.output(TRIG_3, GPIO.LOW)
+            while GPIO.input(ECHO_3) == 0:
+                start_3= time.time()
+            while GPIO.input(ECHO_3) == 1:
+                end_3 = time.time()
+
+            duration_3 = end_3 - start_3
+
+            distance_3 = duration_3 * MEASURE_REFERENCE
+            distanceRound_3 = round(distance_3, 2)
+
+            print("Distance of sensor 3 :", distanceRound_3, "cm")
+
+            left_val = (distanceRound_1<MAX_DISTANCE)
+            mid_val = (distanceRound_2<MAX_DISTANCE)
+            right_val = (distanceRound_3<MAX_DISTANCE)
+            
+            print(left_val,mid_val,right_val)
+
+            return left_val,mid_val,right_val
+
 
     def get_direction(self):
         """
